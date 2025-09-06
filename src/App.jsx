8a3acfadecc3a5145703c642a163a8aeba0db61d
dinesh <dineshk033@gmail.com>
2025-09-06 11:57:04 +0530

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HomeLayout from "./layout";
 
 export default function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <HomeLayout />
+    </div>
+  );
 }
